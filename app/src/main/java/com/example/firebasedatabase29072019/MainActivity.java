b2b802745a4aa49ca09b8bbe5a1661a906d029b0
minhnc02082000 <53040823+minhnc02082000@users.noreply.github.com>
2019-10-14 19:32:46 +0700

    'A'

--- app/src/main/java/com/example/firebasedatabase29072019/MainActivity.java
+++ app/src/main/java/com/example/firebasedatabase29072019/MainActivity.java
@@ -2,15 +2,14 @@ package com.example.firebasedatabase29072019;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     // Write a message to the database
@@ -80,15 +79,29 @@ public class MainActivity extends AppCompatActivity {
 
         //4: Object
 
-        myRef.child("trung tam").setValue(new Thongtin("38 nguyen lam, p6 q10", 01234567)).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if (task.isSuccessful()) {
-                    Toast.makeText(MainActivity.this, "Thanh cong", Toast.LENGTH_SHORT).show();
-                } else {
-                    Log.d("BBB", task.getException().toString());
+//        myRef.child("trung tam").setValue(new Thongtin("38 nguyen lam, p6 q10", 01234567)).addOnCompleteListener(new OnCompleteListener<Void>() {
+//            @Override
+//            public void onComplete(@NonNull Task<Void> task) {
+//                if (task.isSuccessful()) {
+//                    Toast.makeText(MainActivity.this, "Thanh cong", Toast.LENGTH_SHORT).show();
+//                } else {
+//                    Log.d("BBB", task.getException().toString());
+//                }
+//            }
+//        });
+
+        ArrayList<String> arrayNames = new ArrayList<>(Arrays.asList("Minh", "Teo", "Ty", "Suu", "Meo"));
+        ArrayList<String> arrayNames1 = new ArrayList<>(Arrays.asList("Minh", "Teo", "Ty", "Suu", "Meo"));
+        for (String name : arrayNames) {
+            if (name.equals("Minh")) {
+                arrayNames.remove(name);
+                for (String name1 : arrayNames1) {
+                    arrayNames1.remove(name1);
                 }
             }
-        });
+
+        }
+        Log.d("BBB", arrayNames.size() + "");
+        // push hash map
     }
 }
